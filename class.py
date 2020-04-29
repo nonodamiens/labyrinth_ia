7ddf14d622d46490ac6a09d828c6ec3a0311b041
nonodamiens <arnaud.mercier80@gmail.com>
2020-04-29 08:55:35 +0200

    Add class.py file

---
+++ class.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+"""
+Les différentes classes du jeu
+"""
+
+class Joueur:
+    """
+    Le sprite contrôlé par le joueur
+    
+    Le sprite à besoin :
+    - d'un emplacement (coordonnées)
+    """
+
+class lab:
+    """
+    Le plan du labyrinthe
+    """
