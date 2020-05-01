70de10bb0b99f71525914bde27f525bc21054001
nonodamiens <arnaud.mercier80@gmail.com>
2020-05-01 16:35:00 +0200

    add constantes file and window_size constant

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 
 import classes
+import constantes
 import pygame
 from pygame.locals import *
 
 pygame.init()
 
-fenetre = pygame.display.set_mode((200, 200))
+fenetre = pygame.display.set_mode((window_size, window_size))
 
 # Boucle
 continuer = True
